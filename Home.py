b6ae4f9e243950512401aec5aeaa688f12019bd9
Emanuel <63027173+emanuelqnunes@users.noreply.github.com>
2023-11-28 16:39:31 -0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="",
+)
+
+# image_path = 'Proj_curry_company/curry_delivery.jpg'
+image = Image.open('ftc_aula58/curry_delivery.jpg')
+st.sidebar.image(image, width=120)
+
+st.sidebar.markdown( '# Cury Company')
+st.sidebar.markdown( '## Fastest Delivery in Town' )
+# Cria linha horizontal para separar
+st.sidebar.markdown( """___""")
+
+
+st.write("# Curry Company Growth Dashboard")
+
+st.markdown(
+    """
+    ### Como utilizar esse Growtd Dashboard?
+    - Visão Empresa:
+        - Visão Gerencial: Métricas gerais de comportamento.
+        - Visão Tática: Indicadores semanais de crescimento.
+        - Visão Geográfica: Insights de geolocalização.
+    - Visão Entregador:
+        - Acompanhamento dos indicadores semanais de crescimento
+    - Visão Restaurantes:
+        - Indicadores semanais de crescimento dos restaurantes
+    ### Ask for Help
+    - Time de Data Sciense no Discord
+
+    """)
